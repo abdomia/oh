08508f7901184bb7a0007d795f4fc8297f7c1cc6
abdomia <abdoibrahim01358@gmail.com>
2025-12-23 21:01:58 +0200

    feat: make oh recive any number of args

--- src/main.py
+++ src/main.py
@@ -6,15 +6,23 @@ def get_fd(p: Path) -> list[Path]:
     return sorted(conts, key=lambda x: x.is_dir(), reverse=True)
 
 @click.command(help="oh is an ls command but with super power")
-@click.argument("path", required=False)
-def oh(path: str):
-    p = path or './'
-    fds = get_fd(Path(p))
-    for item in fds:
-        if item.is_dir():
-            click.echo(click.style(f"{str(item)}/", fg="green"))
-        else:
-            click.echo(str(item))
+@click.argument(
+    "dest",
+    required=False,
+    nargs=-1
+)
+# TODO make this function pretty print of any number of args/dest
+def oh(dest: tuple[str]):
+    d = dest or '.'
+    for entrys in d:
+        fds = get_fd(Path(entrys))
+        for item in fds:
+            if item.is_dir():
+                click.echo(click.style(f"{str(item)}/", fg="green"))
+            else:
+                click.echo(str(item))
 
+
+# TODO think also of another implementations for this project
 if __name__ == "__main__":
     oh()
